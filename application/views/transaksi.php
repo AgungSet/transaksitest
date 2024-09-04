4d88012fdd83c0cb5f6c39e71e89debde6e141f6
AgungSet <116327716+AgungSet@users.noreply.github.com>
2024-09-04 14:57:06 +0700

    install

---
+++ application/views/transaksi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=10.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HALOO TRANSAKSI</h1>
+    <H2>Halo Agung setiawan</H2>
+
+<main>
+    <h2>Selamat datang di halaman Home</h2>
+    <p>Ini adalah halaman utama dari website Anda.</p>
+    <!-- Konten utama halaman home -->
+</main>
+</footer>
+        <p>&copy; ini footer ya.</p>
+        <!-- Konten footer -->
+    </footer>
+</body>
+</html>
